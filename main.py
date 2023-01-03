7ff319eebd6934402df2d373810dd82811f4c008
Bhavya Dalal <83267251+dalalbhavya@users.noreply.github.com>
2023-01-03 21:21:26 +0530

    basic 3d plot

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as mplot3d
+import numpy as np
+import mpl_toolkits.mplot3d.axes3d as axes3d
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+plt.show()
