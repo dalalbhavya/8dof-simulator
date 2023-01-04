fc35088f1d924fed0cf78710554ad522c961a183
Bhavya Dalal <83267251+dalalbhavya@users.noreply.github.com>
2023-01-04 22:11:40 +0530

    eight link manipulator ready

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as mplot3d
 import numpy as np
 import mpl_toolkits.mplot3d.axes3d as axes3d
-
+LINK_LEN = 100
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
-z = [0, 1, 2]
-x = [0, 2, 3]
-y = [0, 4, 5]
+z = [0, 1, 2, 3, 4, 5, 6, 7]
+x = [0, 2, 3, 5, 7, 10, 14, 10]
+y = [0, 4, 5, 2, 3, 5, 7, 10]
 
-ax.plot(x[:2],y[:2],z[:2], label="link1")
-ax.plot(x[1:], y[1:], z[1:], label = "link2")
+for i in range(len(z)):
+    ax.plot(x[i:i+2], y[i:i+2], z[i:i+2], label="link"+str(i))
 ax.legend()
 
 plt.show()
