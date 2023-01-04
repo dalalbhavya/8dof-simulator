6d21a804f5650a2abf9fd1a61d351e8b01ea392b
Bhavya Dalal <83267251+dalalbhavya@users.noreply.github.com>
2023-01-04 21:58:29 +0530

    added two links

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ import mpl_toolkits.mplot3d.axes3d as axes3d
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
+z = [0, 1, 2]
+x = [0, 2, 3]
+y = [0, 4, 5]
+
+ax.plot(x[:2],y[:2],z[:2], label="link1")
+ax.plot(x[1:], y[1:], z[1:], label = "link2")
+ax.legend()
+
 plt.show()
