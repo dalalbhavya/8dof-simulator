8f708f164ef70ba6d5890d4e10c0c1a190501b18
Bhavya Dalal <dalalbhavya@gmail.com>
2023-01-14 21:51:07 +0530

    added file upload option

--- pages/01_File Upload.py
+++ pages/01_File Upload.py
@@ -5,3 +5,9 @@ st.set_page_config(
 )
 
 st.title("File Upload")
+
+#Add Environment File
+env_config_csv = st.file_uploader("Choose Environment CSV file")
+
+#Add Trajectory File
+traj_csv = st.file_uploader("Choose Trajectory CSV file")
