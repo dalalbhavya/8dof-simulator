d2d787c7789ec423cac1cf1470621711cf9a4440
Bhavya Dalal <dalalbhavya@gmail.com>
2023-01-15 13:17:03 +0530

    image added

--- Homepage.py
+++ Homepage.py
@@ -5,6 +5,7 @@ st.set_page_config(
 )
 
 st.title("Top Gear by IVDC Club IIT Indore")
+st.image("data/full_width.png")
 
 st.text("This page contains how to use this webapp to simulate the robotic arm")
 
