68ee2e3ecff8be979dff97b0c535dd13d5b28fc2
Bhavya Dalal <dalalbhavya@gmail.com>
2023-01-18 00:17:03 +0530

    updated README.md

--- Homepage.py
+++ Homepage.py
@@ -3,9 +3,21 @@ st.set_page_config(
     page_title="Top Gear",
     initial_sidebar_state="expanded"
 )
+readme_path = "README.md"
+readme_file = open(readme_path, "r")
+
+while True:
+    line = readme_file.readline()
+    st.markdown(line)
+
+    if not line:
+        break
+
+readme_file.close()
 
 st.title("Top Gear by IVDC Club IIT Indore")
 st.image("data/full_width.png")
+st.markdown("![foo](README.md, 'title')")
 
 st.text("This page contains how to use this webapp to simulate the robotic arm")
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 8dof-simulator
-In case of issue
+# Top Gear 2023
+![TopGearLogo](/data/full_width.png "title")
+## Using the Simulator
+1. To use the simulator make sure you are connected to the internet
